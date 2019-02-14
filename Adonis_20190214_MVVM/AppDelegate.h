9867782d16aeed3c15b30e16ccabe44fe68d86fb
曹弘扬 <296786475@qq.com>
2019-02-14 16:07:54 +0800

    Initial Commit

---
+++ Adonis_20190214_MVVM/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Adonis_20190214_MVVM
+//
+//  Created by Adonis_HongYang on 2019/2/14.
+//  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Adonis_20190214_MVVM/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Adonis_20190214_MVVM
+//
+//  Created by Adonis_HongYang on 2019/2/14.
+//  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
